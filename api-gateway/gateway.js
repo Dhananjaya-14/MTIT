7fce80fa767cdf5e198760588ca61474964697e6
Dhananjaya-14 <dhananjaya2859dk@gmail.com>
2026-05-02 05:56:27 +0530

    errors

--- api-gateway/gateway.js
+++ api-gateway/gateway.js
@@ -8,7 +8,6 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 registerRoutes(app);
